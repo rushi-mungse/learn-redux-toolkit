da3891a12be687fa3a8475809f6f4701e8d26210
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-04-05 18:01:20 +0530

    adding navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="bg-gray-900">
+      <nav className="container mx-auto flex items-center justify-between py-4">
+        <Link to={"/"}>
+          <h1 className="text-xl text-orange-500">REDUX STORE</h1>
+        </Link>
+        <div className="text-white">
+          <Link
+            to={"/"}
+            className="mr-4 text-lg hover:text-orange-500 duration-300 transition-all"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/cart"}
+            className="mr-4 text-lg hover:text-orange-500 duration-300 transition-all"
+          >
+            Cart
+          </Link>
+          <span className="font-bold text-lg">Items : 0</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
