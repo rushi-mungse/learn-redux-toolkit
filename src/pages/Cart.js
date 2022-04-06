0a5b68b0b5331a716867b0832b1df1a3b0306c0a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-04-06 11:45:03 +0530

    some fix

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,14 +1,36 @@
 import { useSelector } from "react-redux";
-import Product from "../components/Product";
+import { remove } from "../store/cartSlice";
+import { useDispatch } from "react-redux";
 
 const Cart = () => {
   const allItemsInCart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleRemove = (id) => {
+    dispatch(remove(id));
+  };
   return (
-    <div className="bg-gray-200 h-screen">
+    <div className="bg-gray-200 min-h-screen">
       <div className="container mx-auto">
-        <div className="grid grid-cols-4 gap-4 items-center justify-center py-8">
+        <div className="flex items-center justify-center py-8 flex-col">
           {allItemsInCart.map((product) => {
-            return <Product key={product.id} product={product} />;
+            return (
+              <div className="flex items-center justify-between px-4 w-9/12 bg-white my-4 h-24">
+                <div className="flex items-center justify-center  ">
+                  <img src={product.image} alt="" className="h-20" />
+                  <h1 className="text-md ml-4">{product.title}</h1>
+                </div>
+                <div>
+                  <span className="mr-8"> ₹{product.price}</span>
+                  <button
+                    className="text-lg py-1 px-4 bg-indigo-600 rounded-full text-white"
+                    onClick={() => handleRemove(product.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
